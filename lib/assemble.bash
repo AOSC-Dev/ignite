c227298cff57f6611d3e79a68d1a12e3c9a04365
liushuyu <liushuyu011@gmail.com>
2026-08-08 11:44:11 +0800

    fd-aaa: explicitly tell iconv the original encoding is UTF8

--- lib/assemble.bash
+++ lib/assemble.bash
@@ -12,7 +12,7 @@ create_blank_floppy_image() {
     local img_size="$2"
     local img_size_blocks=$((img_size / 1024))
 
-    iconv -t CP437 "${CURDIR}"/assets/non-bootable-msg.txt > "${CURDIR}"/build/non-bootable-msg.bin
+    iconv -f UTF-8 -t CP437 "${CURDIR}"/assets/non-bootable-msg.txt -o "${CURDIR}"/build/non-bootable-msg.bin
     unix2dos "${CURDIR}"/build/non-bootable-msg.bin
     mkfs.vfat -F12 --mbr=n -m "${CURDIR}/build/non-bootable-msg.bin" -n 'AG-BOOT' -C "$img_file" "$img_size_blocks"
 }
